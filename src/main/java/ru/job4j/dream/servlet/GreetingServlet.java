dc34b7b0c7741258d9103bda34e906396d313810
RVohmin <vohmin77@gmail.com>
2020-08-08 08:29:10 +0300

    4. Ajax Jquery [#304514]

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,5 +1,8 @@
 package ru.job4j.dream.servlet;
 
+
+import com.google.gson.JsonObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,9 +23,15 @@ public class GreetingServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain");
         resp.setCharacterEncoding("UTF-8");
+        resp.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+        resp.setHeader("Access-Control-Allow-Methods", "GET");
+        resp.setHeader("Access-Control-Max-Age", "1728000");
+        resp.setHeader("Access-Control-Allow-Origin", "*");
         String name = req.getParameter("name");
+        JsonObject gson = new JsonObject();
+        gson.addProperty("text", "Nice to meet you, " + name + "!");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("Nice to meet you, " + name);
+        writer.println(gson);
         writer.flush();
     }
 }
